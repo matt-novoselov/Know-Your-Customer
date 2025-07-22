6cf4387cdc1a1490469bf4d23d92f71557d0b11f
Matvei Novoselov <matveynovoselov@gmail.com>
2025-07-22 11:13:53 +0200

    Refactored to store only DMY, without time

--- Source/Peaks KYC/Views/Component Views/Input Fields/Date Input Field/DateInputFieldSheet.swift
+++ Source/Peaks KYC/Views/Component Views/Input Fields/Date Input Field/DateInputFieldSheet.swift
@@ -8,31 +8,40 @@
 import SwiftUI
 
 struct DateInputFieldSheet: View {
-    @Environment(\ .dismiss) private var dismiss
-    @State private var tempDate: Date = Date()
-    
+    @Environment(\.dismiss) private var dismiss
     let fieldLabel: String
-    @Binding var selectedDate: Date?
-
+    @Binding var selectedComponents: DateComponents?
+    @State private var tempComponents: DateComponents =
+    Calendar.current.dateComponents([.year, .month, .day], from: Date())
+    
     var body: some View {
         VStack {
             ZStack {
                 Text(fieldLabel)
                     .font(.dazzed(style: .title1, weight: .bold))
                     .frame(maxWidth: .infinity, alignment: .leading)
-
+                
                 Button("Done") {
-                    selectedDate = tempDate
+                    selectedComponents = tempComponents
                     dismiss()
                 }
                 .foregroundStyle(.secondary)
                 .fontWeight(.semibold)
                 .frame(maxWidth: .infinity, alignment: .trailing)
             }
-
+            
+            let dateBinding = Binding<Date>(
+                get: {
+                    Calendar.current.date(from: tempComponents)!
+                },
+                set: { newDate in
+                    tempComponents = Calendar.current.dateComponents([.year, .month, .day], from: newDate)
+                }
+            )
+            
             DatePicker(
                 "Select your \(fieldLabel)",
-                selection: $tempDate,
+                selection: dateBinding,
                 displayedComponents: [.date]
             )
             .datePickerStyle(.graphical)
@@ -40,7 +49,9 @@ struct DateInputFieldSheet: View {
         .padding()
         .padding(.top, 5)
         .onAppear {
-            tempDate = selectedDate ?? Date()
+            if let existing = selectedComponents {
+                tempComponents = existing
+            }
         }
     }
 }
